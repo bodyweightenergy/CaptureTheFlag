d0e2ae8f3d6d6265ad2a3c9e17fd087dc082a763
hsalem <hazim22291@hotmail.com>
2016-08-08 15:45:11 -0500

    initial commit, barebones class for CaptureTheFlag plugin

---
+++ CaptureTheFlag.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Oxide.Core.Plugins;
+using Oxide.Game.Rust.Cui;
+using System.Reflection;
+using System;
+
+namespace Oxide.Plugins
+{
+	//TODO: Set ResourceId to valid number when approved by Oxide admins
+    [Info("CaptureTheFlag", "BodyweightEnergy / AussieHootie", "0.0.1", ResourceId = 0)]
+    class CaptureTheFlag : RustPlugin
+    {
+		// Add all code here.
+	}
+}
